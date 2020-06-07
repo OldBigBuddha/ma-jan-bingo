59093444282851fe05c77551421070c27e59b042
OldBigBuddha <development@oldbigbuddha.net>
2020-06-07 16:35:31 +0900

    [update] Item component
    use useEffect()

--- src/components/Item.component.js
+++ src/components/Item.component.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Item = props => {
   const [value, setValue] = useState(props.value)
   const [isDone, setDone] = useState(false)
+
+  useEffect(() => setValue(props.value), [props.value])
 
   return (
     <div className={"item " + (isDone ? "done" : "")} onClick={() => setDone(true)}>
